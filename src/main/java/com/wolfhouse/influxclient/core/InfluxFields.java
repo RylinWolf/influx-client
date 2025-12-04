24566ab9110cebd06516fb0e98328054e23156d1
RylinWolf <1971257026@qq.com>
2025-12-04 10:05:34 +0800

    feat: 为 Influx 字段、标签 类 新增方法及优化

--- src/main/java/com/wolfhouse/influxclient/core/InfluxFields.java
+++ src/main/java/com/wolfhouse/influxclient/core/InfluxFields.java
@@ -1,29 +1,36 @@
 package com.wolfhouse.influxclient.core;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Set;
+import java.util.SequencedSet;
 
 /**
  * InfluxDB 存储对象字段集合
  *
  * @author Rylin Wolf
  */
+@Slf4j
 public class InfluxFields {
-    private final Map<String, Object> fields;
+    private final LinkedHashMap<String, Object> fields;
 
     private InfluxFields(Map<String, Object> fields) {
-        this.fields = Collections.synchronizedMap(fields);
+        this.fields = (LinkedHashMap<String, Object>) Collections.synchronizedMap(fields);
     }
 
     public InfluxFields() {
-        fields = Collections.synchronizedMap(new LinkedHashMap<>());
+        this.fields = (LinkedHashMap<String, Object>) Collections.synchronizedMap(new LinkedHashMap<String, Object>());
     }
 
     @SafeVarargs
     public static InfluxFields of(Map.Entry<String, Object>... fields) {
         return new InfluxFields(new LinkedHashMap<>(Map.ofEntries(fields)));
+    }
+
+    public static InfluxFields of(LinkedHashMap<String, Object> fields) {
+        return new InfluxFields(fields);
     }
 
     public static InfluxFields instance() {
@@ -35,17 +42,33 @@ public class InfluxFields {
     }
 
     public InfluxFields add(String key, Object value) {
-        fields.put(key, value);
+        fields.putLast(key, value);
         return this;
     }
 
     public InfluxFields addAll(Map<String, Object> fields) {
-        this.fields.putAll(fields);
+        assert fields != null : "【InfluxFields】要填加的字段不得为 null";
+        if (fields.isEmpty()) {
+            return this;
+        }
+        if (!(LinkedHashMap.class.isAssignableFrom(fields.getClass()))) {
+            log.warn("【InfluxFields】要添加的字段是无序的。若是查询操作，应使用有序 Map，否则可能导致结果集映射出错！");
+        }
+        // LinkedHashMap 重写了 forEach，因此无需特殊区分
+        fields.forEach(this::add);
         return this;
     }
 
     public Object get(String key) {
         return fields.get(key);
+    }
+
+    public Map.Entry<String, Object> getFirst() {
+        return fields.firstEntry();
+    }
+
+    public boolean isEmpty() {
+        return fields.isEmpty();
     }
 
     public Object remove(String key) {
@@ -56,8 +79,8 @@ public class InfluxFields {
         return fields.containsKey(key);
     }
 
-    public Set<String> keySet() {
-        return fields.keySet();
+    public SequencedSet<String> keySet() {
+        return fields.sequencedKeySet();
     }
 
     public LinkedHashMap<String, Object> toMap() {
--- src/main/java/com/wolfhouse/influxclient/core/InfluxTags.java
+++ src/main/java/com/wolfhouse/influxclient/core/InfluxTags.java
@@ -1,5 +1,7 @@
 package com.wolfhouse.influxclient.core;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.*;
 
 /**
@@ -7,21 +9,26 @@ import java.util.*;
  *
  * @author Rylin Wolf
  */
+@Slf4j
 public class InfluxTags {
 
-    private final Map<String, String> tags;
+    private final LinkedHashMap<String, String> tags;
 
     public InfluxTags() {
-        tags = Collections.synchronizedMap(new LinkedHashMap<>());
+        tags = (LinkedHashMap<String, String>) Collections.synchronizedMap(new LinkedHashMap<String, String>());
     }
 
     private InfluxTags(Map<String, String> tags) {
-        this.tags = Collections.synchronizedMap(tags);
+        this.tags = (LinkedHashMap<String, String>) Collections.synchronizedMap(tags);
     }
 
     @SafeVarargs
     public static InfluxTags of(Map.Entry<String, String>... tags) {
         return new InfluxTags(new LinkedHashMap<>(Map.ofEntries(tags)));
+    }
+
+    public static InfluxTags of(LinkedHashMap<String, String> fields) {
+        return new InfluxTags(fields);
     }
 
     public static InfluxTags instance() {
@@ -38,7 +45,15 @@ public class InfluxTags {
     }
 
     public InfluxTags addAll(Map<String, String> tags) {
-        this.tags.putAll(tags);
+        assert tags != null : "【InfluxTags】要填加的标签不得为 null";
+        if (tags.isEmpty()) {
+            return this;
+        }
+        if (!(LinkedHashMap.class.isAssignableFrom(tags.getClass()))) {
+            log.warn("【InfluxTags】要添加的标签是无序的。若是查询操作，应使用有序 Map，否则可能导致结果集映射出错！");
+        }
+        // LinkedHashMap 重写了 forEach，因此无需特殊区分
+        tags.forEach(this::add);
         return this;
     }
 
@@ -50,7 +65,15 @@ public class InfluxTags {
         return tags.get(key);
     }
 
-    public String removeTag(String key) {
+    public Map.Entry<String, String> getFirst() {
+        return tags.firstEntry();
+    }
+
+    public boolean isEmpty() {
+        return tags.isEmpty();
+    }
+
+    public String remove(String key) {
         return tags.remove(key);
     }
 
