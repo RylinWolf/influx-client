5af1967c2c6c4fa832b7f5b7e9ce498f272f2a86
RylinWolf <1971257026@qq.com>
2025-12-04 15:47:31 +0800

    feat: 新增字段标签重复异常类

---
+++ src/main/java/com/wolfhouse/influxclient/exception/DuplicateFieldTagException.java
@@ -0,0 +1,26 @@
+package com.wolfhouse.influxclient.exception;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.Arrays;
+
+/**
+ * @author Rylin Wolf
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class DuplicateFieldTagException extends RuntimeException {
+    public static final String DUPLICATE_FIELD_TAG = "重复的标签与字段：";
+
+    public String[] duplicated;
+
+    public DuplicateFieldTagException(String... duplicated) {
+        this(DUPLICATE_FIELD_TAG, duplicated);
+    }
+
+    public DuplicateFieldTagException(String message, String[] duplicated) {
+        super(message + Arrays.toString(duplicated));
+        this.duplicated = duplicated;
+    }
+}
