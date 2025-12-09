5781e6351b1e2f448dedf7401dac448128a122a6
RylinWolf <1971257026@qq.com>
2025-12-09 14:51:55 +0800

    feat: 为 Influx 客户端新增查询并转换方法重载，在不传递指定类型时，默认转换为 map

--- src/main/java/com/wolfhouse/influxclient/client/InfluxClient.java
+++ src/main/java/com/wolfhouse/influxclient/client/InfluxClient.java
@@ -133,30 +133,40 @@ public class InfluxClient {
     /**
      * 使用给定的查询条件包装器和指定的目标类，将查询结果映射为指定类型的集合。
      *
-     * @param <T>     目标类型，必须继承自 AbstractBaseInfluxObj。
+     * @param <E>     目标类型，必须继承自 AbstractBaseInfluxObj。
      * @param wrapper 查询条件包装器，用于构建查询条件。
      * @param clazz   目标类的类型信息，用于映射查询结果。
      * @return 映射后的目标类型集合。
      */
-    public <T extends AbstractBaseInfluxObj> Collection<T> queryMap(InfluxQueryWrapper<?> wrapper, Class<T> clazz) {
+    public <E extends AbstractBaseInfluxObj, T extends AbstractActionInfluxObj> Collection<E> queryMap(InfluxQueryWrapper<T> wrapper, Class<E> clazz) {
         return InfluxObjMapper.mapAll(query(wrapper), clazz, wrapper);
+    }
+
+    /**
+     * 根据给定的查询条件包装器执行查询，并将结果转换为包含键值对的列表形式返回。
+     *
+     * @param wrapper 查询条件包装器，包含查询的条件和参数，用于构建查询语句和设置查询参数。
+     * @return 查询结果的列表，每个列表项为一个映射，表示查询结果中的各列及其对应的值。
+     */
+    public List<Map<String, Object>> queryMap(InfluxQueryWrapper<?> wrapper) {
+        return InfluxObjMapper.compressToMapList(query(wrapper), wrapper);
     }
 
     /**
      * 对给定的查询条件进行分页查询，返回指定类型的分页结果。
      *
-     * @param <T>      数据对象的类型，必须继承自 AbstractBaseInfluxObj。
+     * @param <E>      数据对象的类型，必须继承自 AbstractBaseInfluxObj。
      * @param wrapper  查询条件包装器，用于构建查询的条件和参数。
      * @param clazz    数据对象的目标类型，用于映射查询结果。
      * @param pageNum  当前页码，从1开始。
      * @param pageSize 每页显示的数据条数。
      * @return 包含查询结果的分页对象，包含总记录数、页码、每页大小以及当前页的数据。
      */
-    public <T extends AbstractBaseInfluxObj> InfluxPage<T> pagination(InfluxQueryWrapper<?> wrapper, Class<T> clazz, long pageNum, long pageSize) {
+    public <E extends AbstractBaseInfluxObj, T extends AbstractActionInfluxObj> InfluxPage<E> pagination(InfluxQueryWrapper<T> wrapper, Class<E> clazz, long pageNum, long pageSize) {
         // 验证分页参数
         assert pageNum > 0 && pageSize > 0 : "分页参数配置有误，必须大于 0";
         // 构建分页结果
-        InfluxPage<T> page = InfluxPage.<T>builder()
+        InfluxPage<E> page = InfluxPage.<E>builder()
                                        .total(count(wrapper))
                                        .pageNum(pageNum)
                                        .pageSize(pageSize)
